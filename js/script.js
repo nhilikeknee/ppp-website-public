17a5f4e50c688b645b17149ecfd9e7c739bfdac1
nhilikeknee <56176648+nhilikeknee@users.noreply.github.com>
2020-07-06 13:26:43 -0700

    added hover feature to #about-us-section

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
-let aboutUsButton = document.getElementById("about-us-button");
-aboutUsButton.onclick = () => {};
+//CHANGES BACK AND FORTH FROM BIO PHOTO TO INDIVIDUAL BIO "TEXT"
+let amandaPhoto = document.getElementById("amanda-photo");
+amandaPhoto.onmouseenter = () => {
+  amandaPhoto.src = "../images/mayushbio.png"; //TODO: Change from ghetto image to text
+};
+
+amandaPhoto.onmouseleave = () => {
+  amandaPhoto.src = "../images/fufuplaceholder.png";
+};
+
+let mayaPhoto = document.getElementById("maya-photo");
+mayaPhoto.onmouseenter = () => {
+  mayaPhoto.src = "../images/mayushbio.png";
+};
+
+mayaPhoto.onmouseleave = () => {
+  mayaPhoto.src = "../images/fufuplaceholder.png";
+};
+
+let nhiPhoto = document.getElementById("nhi-photo");
+nhiPhoto.onmouseenter = () => {
+  nhiPhoto.src = "../images/mayushbio.png";
+};
+
+nhiPhoto.onmouseleave = () => {
+  nhiPhoto.src = "../images/fufuplaceholder.png";
+};
