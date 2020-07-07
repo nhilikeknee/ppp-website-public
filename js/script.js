213e97b9056e7cc94d1b8345dcc5ac30d9050e33
Maya Rozenshteyn <rozenshteynm@gmail.com>
2020-07-07 08:33:59 -0700

    the text...it's...it's...real :)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,44 @@
 //CHANGES BACK AND FORTH FROM BIO PHOTO TO INDIVIDUAL BIO "TEXT"
 let amandaPhoto = document.getElementById("amanda-photo");
+let amandaBio = document.getElementById("amanda-bio");
 amandaPhoto.onmouseenter = () => {
-  amandaPhoto.src = "../images/mayushbio.png"; //TODO: Change from ghetto image to text
+  amandaPhoto.style.display = "none";
+  amandaBio.style.display = "block";
 };
-
-amandaPhoto.onmouseleave = () => {
-  amandaPhoto.src = "../images/fufuplaceholder.png";
+amandaBio.onmouseleave = () => {
+  amandaPhoto.style.display = "block";
+  amandaBio.style.display = "none";
 };
 
 let mayaPhoto = document.getElementById("maya-photo");
+let mayaBio = document.getElementById("maya-bio");
 mayaPhoto.onmouseenter = () => {
-  mayaPhoto.src = "../images/mayushbio.png";
+  mayaPhoto.style.display = "none";
+  mayaBio.style.display = "block";
 };
-
-mayaPhoto.onmouseleave = () => {
-  mayaPhoto.src = "../images/fufuplaceholder.png";
+mayaBio.onmouseleave = () => {
+  mayaPhoto.style.display = "block";
+  mayaBio.style.display = "none";
 };
 
 let nhiPhoto = document.getElementById("nhi-photo");
+let nhiBio = document.getElementById("nhi-bio");
 nhiPhoto.onmouseenter = () => {
-  nhiPhoto.src = "../images/mayushbio.png";
+  nhiPhoto.style.display = "none";
+  nhiBio.style.display = "block";
+};
+nhiBio.onmouseleave = () => {
+  nhiPhoto.style.display = "block";
+  nhiBio.style.display = "none";
 };
 
-nhiPhoto.onmouseleave = () => {
-  nhiPhoto.src = "../images/fufuplaceholder.png";
+/* let bioFlip = (initState, finalState) => {
+  initState.style.display = "none";
+  finalState.style.display = "block";
+}
+nhiPhoto.onmouseenter = bioFlip(nhiPhoto, nhiBio); */
+// nhiPhoto.addEventListener("mouseout", bioFlip(nhiBio, nhiPhoto));
+
+const newsletterPopup = () => {
+  timeoutID = window.setTimeout(window.alert, 2*1000, 'That was really slow!');
 };
