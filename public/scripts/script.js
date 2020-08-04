a77c7339254f80d2bacf6846afdc23c1d236607d
nhilikeknee <56176648+nhilikeknee@users.noreply.github.com>
2020-08-04 16:26:06 -0700

    made Contact Us page more Figma-y...and beautified the Join Our Newsletter box but she's still a little :p on that one screen

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,14 +6,14 @@ $(window).bind('scroll', function () {
   if ($(window).scrollTop() > 50) {
     $('.navbar').css({ "position": "fixed", "top": "0px" });
   }
-  else if ($(window).width() > 977) { // SWEETETH I THINK THIS 977 FIXED THAT ONE WACK SCREEN (WAS 992)
+  else if ($(window).width() > 977) { // 977, FIXED THAT ONE WACK SCREEN (WAS 992)
     $('.navbar').css({ "position": "fixed", "top": "100px" });
   }
 });
 
 // TO MAKE THE NAV RENDER PROPERLY ON < 992PX SCREENS
 $(window).resize(function () {
-  if ($(window).width() < 977) { // SWEETETH I THINK THIS 977 FIXED THAT ONE WACK SCREEN (WAS 992)
+  if ($(window).width() < 977) { // 977, FIXED THAT ONE WACK SCREEN (WAS 992)
     $('.navbar').css({ "position": "fixed", "top": "0px" });
   }
   else if ($(window).scrollTop() < 50) {
@@ -32,25 +32,25 @@ let amandaPhoto = document.getElementById("amanda-photo");
 let amandaBio = document.getElementById("amanda-bio");
 
 amandaPhoto.onmouseenter = () => {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 1025) { //1025 for it to work on iPads horizontally
     amandaPhoto.style.display = "none";
     amandaBio.style.display = "block";
   }
 };
 amandaBio.onmouseleave = () => {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 1025) {
     amandaPhoto.style.display = "block";
     amandaBio.style.display = "none";
   }
 };
-amandaPhoto.onclick = () => {  // SWEETETH THIS MAKES THE FUFUS CHANGE PROPERLY ON PHONES/(HOPEFULLY) IPADS
-  if ($(window).width() <= 992) {
+amandaPhoto.onclick = () => {
+  if ($(window).width() <= 1025) {
     amandaPhoto.style.display = "none";
     amandaBio.style.display = "block";
   }
 };
 amandaBio.onclick = () => {
-  if ($(window).width() <= 992) {
+  if ($(window).width() <= 1025) {
     amandaPhoto.style.display = "block";
     amandaBio.style.display = "none";
   }
@@ -59,25 +59,25 @@ amandaBio.onclick = () => {
 let mayaPhoto = document.getElementById("maya-photo");
 let mayaBio = document.getElementById("maya-bio");
 mayaPhoto.onmouseenter = () => {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 1025) {
     mayaPhoto.style.display = "none";
     mayaBio.style.display = "block";
   }
 };
 mayaBio.onmouseleave = () => {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 1025) {
     mayaPhoto.style.display = "block";
     mayaBio.style.display = "none";
   }
 };
 mayaPhoto.onclick = () => {
-  if ($(window).width() <= 992) {
+  if ($(window).width() <= 1025) {
     mayaPhoto.style.display = "none";
     mayaBio.style.display = "block";
   }
 };
 mayaBio.onclick = () => {
-  if ($(window).width() <= 992) {
+  if ($(window).width() <= 1025) {
     mayaPhoto.style.display = "block";
     mayaBio.style.display = "none";
   }
@@ -87,25 +87,25 @@ mayaBio.onclick = () => {
 let nhiPhoto = document.getElementById("nhi-photo");
 let nhiBio = document.getElementById("nhi-bio");
 nhiPhoto.onmouseenter = () => {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 1025) {
     nhiPhoto.style.display = "none";
     nhiBio.style.display = "block";
   }
 };
 nhiBio.onmouseleave = () => {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 1025) {
     nhiPhoto.style.display = "block";
     nhiBio.style.display = "none";
   }
 };
 nhiPhoto.onclick = () => {
-  if ($(window).width() <= 992) {
+  if ($(window).width() <= 1025) {
     nhiPhoto.style.display = "none";
     nhiBio.style.display = "block";
   }
 };
 nhiBio.onclick = () => {
-  if ($(window).width() <= 992) {
+  if ($(window).width() <= 1025) {
     nhiPhoto.style.display = "block";
     nhiBio.style.display = "none";
   }
